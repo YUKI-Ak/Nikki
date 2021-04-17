e3eabb4c838e6e3c89a1890965ee34c3acfd0767
YUKI-Ak <on89.akamatsu.yuki@gmail.com>
2021-04-18 08:19:24 +0900

    トップページ変更

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get users_index_url
+    get articles_index_url
     assert_response :success
   end
 
