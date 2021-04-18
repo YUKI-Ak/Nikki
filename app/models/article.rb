6c7b69d7faebcc4f4d32b849e0d78e80e7b977b7
YUKI-Ak <on89.akamatsu.yuki@gmail.com>
2021-04-18 11:18:20 +0900

    単体テスト用gemインストール

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
 
   with_options presence: true do
-    validates :title
+    validates :title,
+               length: { maximum: 50 }
     validates :content
   end
 end
