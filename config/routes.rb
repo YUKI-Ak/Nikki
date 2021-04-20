b1efe10dfdc95c83cfea6dd78334f48e00b206da
YUKI-Ak <on89.akamatsu.yuki@gmail.com>
2021-04-20 18:27:06 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
-  resources :articles
+  resources :articles, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments
+  end
 end
