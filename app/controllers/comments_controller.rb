32ccccb56de409e94ac69c2939cd55de99e57b25
YUKI-Ak <on89.akamatsu.yuki@gmail.com>
2021-04-21 15:41:30 +0900

    コメント削除機能作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:create, :destroy]
+
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to root_path, notice: "コメントは正常に削除されました。"
+
   end
 
   private
     def comment_params
       params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+    end
+
+    def set_comment
+      @article = Article.find(params[:article_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
   resources :articles, only: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
 end
