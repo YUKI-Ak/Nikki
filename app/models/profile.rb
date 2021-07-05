bb4aa56b2ca9cc37fa83144843ea2dc01be9a8f7
YUKI-Ak <on89.akamatsu.yuki@gmail.com>
2021-07-05 13:30:31 +0900

    バリデーション作成

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,8 @@
 class Profile < ApplicationRecord
+  belongs_to :profile
+
+  with_options presence: true do
+    validates :about_me
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
+  belongs_to :profile
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
